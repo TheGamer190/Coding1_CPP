b0fc100c734ad43053271b23db9634079a733056
Derek Richards <luke19pro@gmail.com>
2026-03-26 10:36:45 -0500

    Creating a simple program

---
+++ Main.cpp
@@ -0,0 +1,22 @@
+//Derek Richards
+
+//To compile open CMD VS 22, 
+// navigate to this folder, 
+// run the command "cl /EHsc " to compile, 
+// run file by calling the name
+
+#include <iostream> //Libary for in out stream for printing text 
+//#include <string>
+using namespace std; //Dont need to type std::
+
+int main()
+{
+    string playerName;
+
+    cout << "What's your name? " << endl;
+    cin >> playerName;
+
+    cout << "Hello " << playerName << "." << endl;
+
+    return 0;
+}
